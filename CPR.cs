39a6b870a516174692d9dca7b1e77b4a0b02a7f8
Nicholas Christiansen <salomangook@gmail.com>
2022-04-26 13:20:48 +0200

    Add project files.

---
+++ CPR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPR_nummer_opgave
+{
+    static internal class CPR
+    {
+        static public bool valdierCPR(string date, string nummer)
+        {
+            DateTime d;
+            int nummer_int;
+            if (DateTime.TryParseExact(date, new string[] { "dd/MM/yy" }, CultureInfo.InvariantCulture, DateTimeStyles.None, out d)
+                && nummer.Length == 4 && int.TryParse(nummer, out nummer_int))
+            {
+                    Console.WriteLine($"{date}-{nummer}");
+                    return true;
+            }
+            else
+            {
+                Console.WriteLine($"{date}-{nummer}");
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using CPR_nummer_opgave;
+do
+{
+    Console.WriteLine("Indtast CPR nummer");
+    Console.WriteLine("Indtast dagen du var født");
+    string dag = Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("Indtast måned du var født");
+    string moened = Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("Indtast år du var født");
+    string aar = Console.ReadLine();
+    Console.Clear();
+
+    Console.WriteLine("Indtast 4 sidste nummer i CPR");
+    string nummer = Console.ReadLine();
+    Console.Clear();
+
+    if (aar.Length > 2)
+    {
+        aar = aar.Substring(2, 2);
+    }
+
+    string date = $"{dag}/{moened}/{aar}";
+
+
+
+    if (!CPR.valdierCPR(date, nummer))
+    {
+        Console.WriteLine("Ugyldigt, Prøv igen");
+    }
+
+    Console.WriteLine("Press 'x' to exit");
+
+    string input = Console.ReadLine();
+    if (input.ToLower() == "x")
+        break;
+
+    Console.Clear();
+} while (true);
